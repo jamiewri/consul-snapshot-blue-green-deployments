0ca501a27796c043685f87ebcba12214aa98021e
jamiewri <jamiewri@me.com>
2021-08-09 09:35:38 +1000

    removing external reference from loadblancer

--- apps/consul/scripts/create-example-kv.sh
+++ apps/consul/scripts/create-example-kv.sh
@@ -3,8 +3,8 @@ CONSUL_IP=$(kubectl get svc consul-server-external  -o json | jq -r '.status.loa
 export CONSUL_HTTP_ADDR="http://${CONSUL_IP}:8500"
 
 echo 'Adding KVs'
-consul kv put internalEnabled true
-consul kv put internalWeight 100
+#consul kv put internalEnabled true
+#consul kv put internalWeight 100
 
 consul kv put payments/
 consul kv put payments/blueEnabled true
