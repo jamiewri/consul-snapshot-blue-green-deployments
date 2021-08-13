391fab7709e7d7f8dafda1fcb41f1e340a90e439
jamiewri <jamiewri@me.com>
2021-08-13 10:35:31 +1000

    cleaning demo

---
+++ README.md
@@ -0,0 +1,31 @@
+# Consul Blue Green Deployments Demo
+
+This repos deploys Consul and a app as show below.
+
+
+## Prerequisites
+- Working Kubernetes environment.
+- `kubectl` binary.
+- `helm` binary.
+
+## Installation
+I'm using the `demo.sh` as a wrapper around some helm commands, to make the demo easier to drive.
+```
+sh demo.sh install consul
+```
+
+> :warning: **Wait for Consul to finish initalizing and for Consul services to have their IPs assigned before continuing**
+
+```
+sh demo.sh install consul-config
+sh demo.sh install monitoring
+sh demo.sh install payments-blue
+sh demo.sh install payments-green
+sh demo.sh install loadbalancer
+sh demo.sh install frontend
+
+```
+
+
+
+
--- demo.sh
+++ demo.sh
@@ -1,17 +1,92 @@
-#/bin/bash
+#!/bin/bash
 
 case $1 in
-  consul)
-    helm install -f ./apps/consul/values.yaml consul hashicorp/consul --version "0.32.1" --wait
-    kubectl create -f ./apps/consul/service.consul-dns-internal.yaml
-    kubectl create -f ./apps/consul/service.consul-server-internal.yaml
-    kubectl create -f ./apps/consul/service.consul-server-external.yaml
-    sh ./apps/consul/scripts/configure-kube-dns.sh
-    sh ./apps/consul/scripts/create-example-kv.sh
+  install)
+
+    case $2 in
+      consul)
+        echo 'Installing consul'
+        helm install -f ./apps/consul/values.yaml consul hashicorp/consul --version "0.32.1" --wait
+        kubectl create -f ./apps/consul/service.consul-dns-internal.yaml
+        kubectl create -f ./apps/consul/service.consul-server-internal.yaml
+        kubectl create -f ./apps/consul/service.consul-server-external.yaml
+
+      ;;
+      consul-config)
+        sh ./apps/consul/scripts/configure-kube-dns.sh
+        sh ./apps/consul/scripts/create-example-kv.sh
+
+      ;;
+      payments-blue)
+        echo 'Installing payments-blue'
+        helm install -f ./apps/payments/deploy/values.yaml payments-blue --set deployment.tag=1 --set appName=payments-blue ./apps/payments/deploy --wait
+
+      ;;
+      payments-green)
+        echo 'Installing payments-green'
+        helm install -f ./apps/payments/deploy/values.yaml payments-green --set deployment.tag=2 --set appName=payments-green ./apps/payments/deploy --wait
+
+      ;;
+      loadbalancer)
+         echo 'Installing loadbalancer'
+         helm install -f ./apps/loadbalancer/values.yaml loadbalancer ./apps/loadbalancer --wait
+
+      ;;
+      frontend)
+        echo 'Installing frontend'
+        helm install -f ./apps/frontend/deploy/values.yaml frontend ./apps/frontend/deploy
+
+      ;;
+      monitoring)
+        echo 'Installing monitoring'
+        helm install -f ./apps/prometheus/values.yaml prometheus prometheus-community/prometheus --version "14.0.0" --wait
+        helm install -f ./apps/grafana/values.yaml grafana grafana/grafana --version "6.9.1" --wait
+
+      ;;
+      dns-test)
+        echo 'Installing dns-test'
+        kubectl apply -f ./apps/dns-test/deployment.dns-test.yaml
+
+      ;;
+      *)
+        echo 'Unknown app!'
+
+      ;;
+    esac
   ;;
 
-  consul-upgrade)
-     helm upgrade -f ./apps/consul/values.yaml consul hashicorp/consul
+  upgrade)
+    case $2 in
+      consul)
+        echo 'Upgrading consul'
+        helm upgrade -f ./apps/consul/values.yaml consul hashicorp/consul
+
+      ;;
+      payments-blue)
+        echo 'Upgrading payments-blue'
+        helm upgrade -f ./apps/payments/deploy/values.yaml payments-blue --set deployment.tag=1 --set appName=payments-blue ./apps/payments/deploy --reset-values
+
+      ;;
+      payments-green)
+        echo 'Upgrading payments-green'
+        helm upgrade -f ./apps/payments/deploy/values.yaml payments-green --set deployment.tag=2 --set appName=payments-green ./apps/payments/deploy --reset-values
+
+      ;;
+      loadbalancer)
+         echo 'Upgrading loadbalancer'
+         helm upgrade -f ./apps/loadbalancer/values.yaml loadbalancer ./apps/loadbalancer
+
+      ;;
+      frontend)
+        echo 'Upgrading frontend'
+        helm upgrade -f ./apps/frontend/deploy/values.yaml frontend ./apps/frontend/deploy --reset-values
+
+      ;;
+      *)
+        echo 'Unknown app!'
+
+      ;;
+    esac
   ;;
 
   info)
@@ -19,74 +94,7 @@ case $1 in
     echo "export CONSUL_HTTP_ADDR=http://${CONSUL_IP}:8500"
   ;;
 
-  dns-test)
-    kubectl apply -f ./apps/consul/deployment.dns-test.yaml
-  ;;
-
-  payments)
-    helm install -f ./apps/payments/values.yaml payments ./apps/payments --wait
-  ;;
-
-  payments-upgrade)
-    helm upgrade -f ./apps/payments/values.yaml payments ./apps/payments 
-  ;;
-
-  upgrade-1)
-    helm upgrade -f ./apps/payments/values.yaml payments ./apps/payments --set deployment.tag=1 --reset-values
-  ;;
-
-  upgrade-2)
-    helm upgrade -f ./apps/payments/values.yaml payments ./apps/payments --set deployment.tag=2 --reset-values
-  ;;
-
-  loadbalancer)
-    helm install -f ./apps/loadbalancer/values.yaml loadbalancer ./apps/loadbalancer --wait
-  ;;
-
-  loadbalancer-upgrade)
-    helm upgrade -f ./apps/loadbalancer/values.yaml loadbalancer ./apps/loadbalancer
-  ;;
-
-  frontend)
-    helm install -f ./apps/frontend/values.yaml frontend ./apps/frontend 
-  ;;
-
-  frontend-upgrade)
-    helm upgrade -f ./apps/frontend/values.yaml frontend ./apps/frontend --reset-values
-  ;;
-
-  monitoring)
-    helm install -f ./apps/prometheus/values.yaml prometheus prometheus-community/prometheus --version "14.0.0" --wait
-    helm install -f ./apps/grafana/values.yaml grafana grafana/grafana --version "6.9.1" --wait
-  ;;
-
-  all)
-    helm install -f ./apps/payments-n/values.yaml payments-green --set deployment.tag=2 --set appName=payments-green ./apps/payments-n --wait
-    helm install -f ./apps/payments-n/values.yaml payments-blue --set deployment.tag=1 --set appName=payments-blue ./apps/payments-n --wait
-    helm install -f ./apps/loadbalancer/values.yaml loadbalancer ./apps/loadbalancer --wait
-    helm install -f ./apps/frontend/values.yaml frontend ./apps/frontend 
-    helm install -f ./apps/prometheus/values.yaml prometheus prometheus-community/prometheus --version "14.0.0"
-    helm install -f ./apps/grafana/values.yaml grafana grafana/grafana --version "6.9.1"
-  ;;
-
-  payments-blue)
-     helm install -f ./apps/payments-n/values.yaml payments-blue --set deployment.tag=1 --set appName=payments-blue ./apps/payments-n --wait
-  ;;
-
-  payments-green-upgrade)
-     helm upgrade -f ./apps/payments-n/values.yaml payments-green --set deployment.tag=2 --set appName=payments-green ./apps/payments-n --reset-values
-  ;;
-
-  payments-blue-upgrade)
-     helm upgrade -f ./apps/payments-n/values.yaml payments-blue --set deployment.tag=1 --set appName=payments-blue ./apps/payments-n --reset-values
-  ;;
-
-  payments-green)
-     helm install -f ./apps/payments-n/values.yaml payments-green --set deployment.tag=2 --set appName=payments-green ./apps/payments-n --wait
+  *)
+    echo 'Unknown action!'
   ;;
 esac
-
-
-# build consul with poyhton  
-#docker  build -t 'jamiewri/consul-python:1.10.1' .
-
