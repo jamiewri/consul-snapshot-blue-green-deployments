7c51ed6e1fc59adba1e2bd188eb251632158646f
jamiewri <jamiewri@me.com>
2021-08-13 11:00:16 +1000

    remooving old templkates

--- apps/payments/build/payments.py
+++ apps/payments/build/payments.py
@@ -1,11 +1,3 @@
-#from flask import Flask, render_template, request
-#import requests
-#import json
-#import random
-#import os
-#import socket
-#import sys
-
 from flask import Flask, jsonify
 
 app = Flask(__name__)
